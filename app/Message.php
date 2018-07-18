f05c735b22c46fc71a79f8448a0d4a65367b5b2e
ZhenXun Wong <zhenxun_9119@hotmail.com>
2018-07-18 10:38:53 +0800

    add relationship Message to user

--- app/Message.php
+++ app/Message.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Message extends Model
 {
     protected $fillable = ['message'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
