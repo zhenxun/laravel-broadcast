e9e8323f589313df314ab4048e82e4d607aa938c
ZhenXun Wong <zhenxun_9119@hotmail.com>
2018-07-18 10:53:34 +0800

    add chat vue components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,10 +13,38 @@ window.Vue = require('vue');
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
+ * 
+ * Vue.component('example-component', require('./components/ExampleComponent.vue'));
+ * 
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+Vue.component('chat-messages', require('./components/ChatMessages.vue'));
+Vue.component('chat-form', require('./components/ChatForm.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        messages: []
+    },
+
+    created() {
+        this.fetchMessages();
+    },
+
+    methods: {
+        fetchMessages() {
+            axios.get('/messages').then(response => {
+                this.messages = response.data;
+            });
+        },
+
+        addMessage(message) {
+            this.messages.push(message);
+
+            axios.post('/messages', message).then(response => {
+                console.log(response.data);
+            });
+        }
+    }
 });
